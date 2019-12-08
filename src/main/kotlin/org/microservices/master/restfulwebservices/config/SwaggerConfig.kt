702b4240132c2cad061a0e96c47c34fa3fcb5f85
cinexin <migueguillamon@gmail.com>
2019-12-08 14:05:20 +0100

    Fix Springboot version since it's not fully compatible with Swagger Enable Spring Actuator

--- src/main/kotlin/org/microservices/master/restfulwebservices/config/SwaggerConfig.kt
+++ src/main/kotlin/org/microservices/master/restfulwebservices/config/SwaggerConfig.kt
@@ -2,6 +2,7 @@ package org.microservices.master.restfulwebservices.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.service.ApiInfo
 import springfox.documentation.service.Contact
 import springfox.documentation.spi.DocumentationType
@@ -19,19 +20,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 class SwaggerConfig {
 
     private val DEFAULT_CONTACT = Contact(
-            "Migue Guillamón",  "" +
+            "Migue Guillamón",
             "https://github.com/cinexin",
             "migueguillamon@gmail.com"
     )
-    private val DEFAULT_API_INFO = ApiInfo(
-            "Api Documentation",
-            "Api Documentation for Master Microservices Demo",
-            "1.0",
-            "http://www.apache.org/licenses/LICENSE-2.0",
-            DEFAULT_CONTACT,
-            "Apache 2.0",
-            "http://www.apache.org/licenses/LICENSE-2.0"
-    )
+    private val DEFAULT_API_INFO =
+            ApiInfoBuilder()
+                    .title("Api Documentation for Master Microservices Demo")
+                    .description("Api Documentation")
+                    .contact(DEFAULT_CONTACT)
+                    .license("Apache 2.0")
+                    .termsOfServiceUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                    .version("1.0")
+                    .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                    .build()
+
     private val DEFAULT_PRODUCES_AND_CONSUMES = hashSetOf(
             "application/json",
             "application/xml"
--- src/main/kotlin/org/microservices/master/restfulwebservices/controller/HelloWorldController.kt
+++ src/main/kotlin/org/microservices/master/restfulwebservices/controller/HelloWorldController.kt
@@ -3,8 +3,10 @@ package org.microservices.master.restfulwebservices.controller
 import org.microservices.master.restfulwebservices.domain.HelloWorldBean
 import org.springframework.context.MessageSource
 import org.springframework.context.i18n.LocaleContextHolder
-import org.springframework.web.bind.annotation.*
-import java.util.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("hello-world")
